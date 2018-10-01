83b3aed1c9b8520ee29a37ea9f9e5114cc69da4c
revolverz <43179700+revolverz@users.noreply.github.com>
2018-10-01 12:42:43 +0400

    Update plugin-menu.js

--- plugin-menu.js
+++ plugin-menu.js
@@ -13,107 +13,111 @@ $(document).ready(function() {
 
     (function($) {
 
-            var methods = {
+        var methods = {
 
-                    init: function(options) {
-                      
-                        return this.each(function() {
-                                         
-                            var menu_selector = "." + $(this).attr('class').split(' ')[0];
+            init: function(options) {
 
-                            // Plugin default settings
-                            var settings = $.extend({
-                                color_default: "",
-                                color_active: "",
-                                time: 500,
-                                background_color: "red",
-                                opacity: "0.9",
-                                width: "80%",
-                                margin_left: "auto",
-                                margin_right: "auto",
-                                border_radius: 0,
-                                onClick: true,
-                                onScroll: true,
-                                onLight_bg: true   
-                            }, options);
+                return this.each(function() {
 
-                            function onScroll() {
-                                var scroll_top = $(document).scrollTop();
-                                $(menu_selector + " a").each(function() {
-                                    var hash = $(this).attr("href");
-                                    var target = $(hash);
-                                    if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
-                                        $(menu_selector + " a").css({
-                                            color: settings.color_default
-                                        });
-                                        $(this).css({
-                                            color: settings.color_active
-                                        });
-                                    } else {
-                                        $(this).css({
-                                            color: settings.color_default
-                                        });
-                                    }
+                    var menu_selector = "." + $(this).attr('class').split(' ')[0];
+
+                    // Plugin default settings
+                    var settings = $.extend({
+                        color_default: "",
+                        color_active: "",
+                        time: 500,
+                        background_color: "red",
+                        opacity: "0.9",
+                        width: "80%",
+                        margin_left: "auto",
+                        margin_right: "auto",
+                        border_radius: 0,
+                        onClick: true,
+                        onScroll: true,
+                        onLight_bg: true
+                    }, options);
+
+                    function onScroll() {
+                        var scroll_top = $(document).scrollTop();
+                        $(menu_selector + " a").each(function() {
+                            var hash = $(this).attr("href");
+                            var target = $(hash);
+                            if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+                                $(menu_selector + " a").css({
+                                    color: settings.color_default
                                 });
-                            };
 
-                            function onClick() {
-                                $("a[href*='#']", this).click(function(e) {
-                                    e.preventDefault();
-                                    $(document).off("scroll");
-                                    $(menu_selector + " a").css({
-                                        color: settings.color_default
-                                    });
-                                    $(this).css({
-                                        color: settings.color_active
-                                    });
-                                    var hash = $(this).attr("href");
-                                    var target = $(hash);
-                                    $("html, body").animate({
-                                        scrollTop: target.offset().top
-                                    }, settings.time, function() {
-                                        window.location.hash = hash;
-                                        $(document).on("scroll", onScroll);
-                                    });
+                                $(this).css({
+                                    color: settings.color_active
                                 });
-                            };
-
-                            var $menu = $(this);
-                            function onLight_bg() {
-                                var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-                                if (scrolled > 0) {
-                                    $menu.css({
-                                        "background-color": settings.background_color,
-                                        "opacity": settings.opacity,
-                                        "width": settings.width,
-                                        "border-radius": settings.border_radius,
-                                        "margin-right": settings.margin_right,
-                                        "margin-left": settings.margin_left
-                                    });
-
-                                } else {
-                                    $menu.css({ "background-color": "" });
-                                }
-                            };
-
-                            function setHandlers(options, listHandlers) {
-                                listHandlers.forEach(function(item) {
-                                    if (options[item.name]) {
-                                        $(document).bind('scroll.menuLighting', item);
-                                    } else {
-                                        $(document).unbind('scroll.menuLighting', item);
-                                    }
-
+                            } else {
+                                $(this).css({
+                                    color: settings.color_default
                                 });
-                            }; setHandlers(settings, [onScroll, onClick, onLight_bg]);
+                            }
                         });
-                },
+                    };
 
-                destroy: function(options) {
-                    return this.each(function() {
-                        $(window).unbind('.menuLighting');
-                    })
-                }
+                    $(document).on("scroll", onScroll);
+                    function onClick() {
+                        $("a[href*='#']", this).click(function(e) {
+                            e.preventDefault();
+                            $(document).off("scroll");
+                            $(menu_selector + " a").css({
+                                color: settings.color_default
+                            });
+                            $(this).css({
+                                color: settings.color_active
+                            });
+                            var hash = $(this).attr("href");
+                            var target = $(hash);
+                            $("html, body").animate({
+                                scrollTop: target.offset().top
+                            }, settings.time, function() {
+                                window.location.hash = hash;
+                               
+                            });
+                        });
+                    };
+
+                    var $menu = $(this);
+
+                    function onLight_bg() {
+                        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+                        if (scrolled > 0) {
+                            $menu.css({
+                                "background-color": settings.background_color,
+                                "opacity": settings.opacity,
+                                "width": settings.width,
+                                "border-radius": settings.border_radius,
+                                "margin-right": settings.margin_right,
+                                "margin-left": settings.margin_left
+                            });
+
+                        } else {
+                            $menu.css({ "background-color": "" });
+                        }
+                    };
+
+                    function setHandlers(options, listHandlers) {
+                        listHandlers.forEach(function(item) {
+                            if (options[item.name]) {
+                                $(document).bind('scroll.menuLighting', item);
+                            } else {
+                                $(document).unbind('scroll.menuLighting', item);
+                            }
+
+                        });
+                    };
+                    setHandlers(settings, [onScroll, onClick, onLight_bg]);
+                });
+            },
+
+            destroy: function(options) {
+                return this.each(function() {
+                    $(window).unbind('.menuLighting');
+                })
+            }
         };
 
         $.fn.menuLighting = function(method) {
